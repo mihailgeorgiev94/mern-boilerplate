c788fb7f97fe76f6bd4f8ebc522b32c23aa5201f
Mihail <mihailgeorgiev94@gmail.com>
2020-04-15 16:37:08 +0300

    Add prod config

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const items = require('./routes/api/items')
 
 const app = express()
 
+// middleware
 app.use(express.json())
 
-
+// db
 const db = require('./config/keys').mongoURI
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
   .then(() => console.log('DB connected'))
   .catch(err => console.log(err))
 
+// routes
 app.use('/api/items', items)
+
+// serve static assets if production
+if(process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => `Connected on port: ${PORT}`)
